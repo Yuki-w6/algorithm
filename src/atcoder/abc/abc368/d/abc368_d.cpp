8ccbc0310dffbd00b26f7df212dffd81924e92fb
yuki-w6 <m1610600@gmail.com>
2024-08-25 20:53:22 +0900

    abc368 D

--- src/atcoder/abc/abc368/d/abc368_d.cpp
+++ src/atcoder/abc/abc368/d/abc368_d.cpp
@@ -1,6 +1,6 @@
 #include <bits/stdc++.h>
-using namespace std;
 #include <atcoder/all>
+using namespace std;
 using namespace atcoder;
 #define rep(i, n) for (int i = 0; i < (int)(n); i++)
 using ll = long long;
@@ -8,11 +8,43 @@ using P = pair<int, int>;
 
 int main()
 {
-    int n;
-    cin >> n;
+    int n, k;
+    cin >> n >> k;
+    vector<vector<int>> to(n);
+    rep(i, n - 1)
+    {
+        int a, b;
+        cin >> a >> b;
+        --a;
+        --b;
+        to[a].push_back(b);
+        to[b].push_back(a);
+    }
+    vector<int> selected(k);
+    rep(i, k) cin >> selected[i], selected[i]--;
+    vector<bool> isSelected(n);
+    rep(i, k) isSelected[selected[i]] = true;
+
+    vector<int> num(n);
+    vector<bool> isVisited(n);
+    auto dfs = [&](auto dfs, int v) -> void
+    {
+        isVisited[v] = true;
+        if (isSelected[v])
+            num[v]++;
+        for (int u : to[v])
+        {
+            if (isVisited[u])
+            {
+                continue;
+            }
+            dfs(dfs, u);
+            num[v] += num[u];
+        }
+    };
+    dfs(dfs, selected[0]);
 
     int ans = 0;
-
+    rep(i, n) if (num[i] > 0) ans++;
     cout << ans << endl;
-    return 0;
 }
