6a2924d2c6486cc5bd22d02c549ceea72a533c86
yuki-w6 <m1610600@gmail.com>
2024-08-31 10:01:57 +0900

    task

---
+++ src/atcoder/abc/abc366/c/abc366_c.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+#include <atcoder/all>
+using namespace std;
+using namespace atcoder;
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+using ll = long long;
+
+int main()
+{
+    int n;
+    cin >> n;
+
+    int ans = 0;
+
+    cout << ans << endl;
+    return 0;
+}
