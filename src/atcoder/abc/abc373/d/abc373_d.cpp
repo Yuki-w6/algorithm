16a485f9cf2709dda21f2440d6317dd632f6c51a
Yuki-w6 <m1610600@gmail.com>
2024-09-29 22:17:15 +0900

    abc373

--- src/atcoder/abc/abc373/d/abc373_d.cpp
+++ src/atcoder/abc/abc373/d/abc373_d.cpp
@@ -5,13 +5,56 @@ using namespace atcoder;
 #define rep(i, n) for (int i = 0; i < (int)(n); i++)
 using ll = long long;
 
+struct Edge
+{
+    int to, w;
+};
+
 int main()
 {
-    int n;
-    cin >> n;
+    int n, m;
+    cin >> n >> m;
+    vector<vector<Edge>> g(n);
+    rep(i, m)
+    {
+        int u, v, w;
+        cin >> u >> v >> w;
+        u--;
+        v--;
+        g[u].emplace_back(v, w);
+        g[v].emplace_back(u, -w);
+    }
 
-    int ans = 0;
+    vector<bool> selected(n);
+    vector<ll> ans(n);
 
-    cout << ans << endl;
+    rep(i, n)
+    {
+        if (selected[i])
+        {
+            continue;
+        }
+        auto dfs = [&](auto dfs, int v, ll val)
+        {
+            if (selected[v])
+            {
+                return;
+            }
+            selected[v] = true;
+            ans[v] = val;
+            for (Edge e : g[v])
+            {
+                dfs(dfs, e.to, val + e.w);
+            }
+            dfs(dfs, i, 0);
+        };
+    }
+
+    rep(i, n)
+    {
+        cout << ans[i] << ' ';
+    }
+    cout << endl;
+
     return 0;
 }
